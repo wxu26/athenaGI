wxu26/athenaGI

commit 9d803bef07cc4bb376b0768cd0da4895b128b709

Remove all redunddant ncellsN calculations from pgen/ files
Don't use delete on Real [] array in history.cpp; instead use a unique_ptr

src/pgen/cpaw.cpp
@@ -224,15 +224,12 @@ void Mesh::UserWorkAfterLoop(ParameterInput *pin) {
 //========================================================================================
 
 void MeshBlock::ProblemGenerator(ParameterInput *pin) {
-  AthenaArray<Real> a1,a2,a3;
-  int nx1 = (ie-is)+1 + 2*NGHOST;
-  int nx2 = (je-js)+1 + 2*NGHOST;
-  int nx3 = (ke-ks)+1 + 2*NGHOST;
-  a1.NewAthenaArray(nx3,nx2,nx1);
-  a2.NewAthenaArray(nx3,nx2,nx1);
-  a3.NewAthenaArray(nx3,nx2,nx1);
+  AthenaArray<Real> a1, a2, a3;
+  a1.NewAthenaArray(ncells3, ncells2, ncells1);
+  a2.NewAthenaArray(ncells3, ncells2, ncells1);
+  a3.NewAthenaArray(ncells3, ncells2, ncells1);
 
-  int level=loc.level;
+  int level = loc.level;
   // Initialize components of the vector potential
   if (block_size.nx3 > 1) {
     for (int k=ks; k<=ke+1; k++) {

src/pgen/field_loop.cpp
@@ -56,13 +56,10 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
   Real gm1 = peos->GetGamma() - 1.0;
   Real iso_cs =peos->GetIsoSoundSpeed();
 
-  AthenaArray<Real> ax,ay,az;
-  int nx1 = (ie-is)+1 + 2*NGHOST;
-  int nx2 = (je-js)+1 + 2*NGHOST;
-  int nx3 = (ke-ks)+1 + 2*NGHOST;
-  ax.NewAthenaArray(nx3,nx2,nx1);
-  ay.NewAthenaArray(nx3,nx2,nx1);
-  az.NewAthenaArray(nx3,nx2,nx1);
+  AthenaArray<Real> ax, ay, az;
+  ax.NewAthenaArray(ncells3, ncells2, ncells1);
+  ay.NewAthenaArray(ncells3, ncells2, ncells1);
+  az.NewAthenaArray(ncells3, ncells2, ncells1);
 
   // Read initial conditions, diffusion coefficients (if needed)
   Real rad = pin->GetReal("problem","rad");

src/pgen/field_loop_poles.cpp
@@ -116,17 +116,14 @@ void Mesh::InitUserMeshData(ParameterInput *pin) {
 //========================================================================================
 
 void MeshBlock::ProblemGenerator(ParameterInput *pin) {
-  Real rad, phi, z;
+  // Real rad, phi, z;
   Real v1, v2, v3;
   // Set initial magnetic fields
   if (MAGNETIC_FIELDS_ENABLED) {
-    AthenaArray<Real> a1,a2,a3;
-    int nx1 = (ie-is)+1 + 2*NGHOST;
-    int nx2 = (je-js)+1 + 2*NGHOST;
-    int nx3 = (ke-ks)+1 + 2*NGHOST;
-    a1.NewAthenaArray(nx3,nx2,nx1);
-    a2.NewAthenaArray(nx3,nx2,nx1);
-    a3.NewAthenaArray(nx3,nx2,nx1);
+    AthenaArray<Real> a1, a2, a3;
+    a1.NewAthenaArray(ncells3, ncells2, ncells1);
+    a2.NewAthenaArray(ncells3, ncells2, ncells1);
+    a3.NewAthenaArray(ncells3, ncells2, ncells1);
 
     int level=loc.level;
     for (int k=ks; k<=ke+1; k++) {
@@ -184,10 +181,10 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
     }
 
     // Initialize interface fields
-    AthenaArray<Real> area,len,len_p1;
-    area.NewAthenaArray(nx1);
-    len.NewAthenaArray(nx1);
-    len_p1.NewAthenaArray(nx1);
+    AthenaArray<Real> area, len, len_p1;
+    area.NewAthenaArray(ncells1);
+    len.NewAthenaArray(ncells1);
+    len_p1.NewAthenaArray(ncells1);
 
     // for 1,2,3-D
     for (int k=ks; k<=ke; ++k) {

src/pgen/linear_wave.cpp
@@ -188,14 +188,14 @@ void Mesh::UserWorkAfterLoop(ParameterInput *pin) {
   // Initialize errors to zero
   Real l1_err[NHYDRO+NFIELD],max_err[NHYDRO+NFIELD];
   for (int i=0; i<(NHYDRO+NFIELD); ++i) {
-    l1_err[i]=0.0;
-    max_err[i]=0.0;
+    l1_err[i] = 0.0;
+    max_err[i] = 0.0;
   }
 
   MeshBlock *pmb = pblock;
   BoundaryValues *pbval;
   while (pmb != nullptr) {
-    pbval=pmb->pbval;
+    BoundaryValues *pbval = pmb->pbval;
     int il=pmb->is, iu=pmb->ie, jl=pmb->js, ju=pmb->je, kl=pmb->ks, ku=pmb->ke;
     // adjust loop limits for fourth order error calculation
     //------------------------------------------------
@@ -210,15 +210,11 @@ void Mesh::UserWorkAfterLoop(ParameterInput *pin) {
     }
     // Save analytic solution of conserved variables in 4D scratch array
     AthenaArray<Real> cons_;
-    int ncells1 = pmb->block_size.nx1 + 2*NGHOST;
-    int ncells2 = 1, ncells3 = 1;
-    if (pmb->block_size.nx2 > 1) ncells2 = pmb->block_size.nx2 + 2*NGHOST;
-    if (pmb->block_size.nx3 > 1) ncells3 = pmb->block_size.nx3 + 2*NGHOST;
     // Even for MHD, there are only cell-centered mesh variables
     int ncells4 = NHYDRO + NFIELD;
     int nl = 0;
-    int nu = ncells4-1;
-    cons_.NewAthenaArray(ncells4, ncells3, ncells2, ncells1);
+    int nu = ncells4 - 1;
+    cons_.NewAthenaArray(ncells4, pmb->ncells3, pmb->ncells2, pmb->ncells1);
 
     //  Compute errors at cell centers
     for (int k=kl; k<=ku; k++) {
@@ -269,7 +265,7 @@ void Mesh::UserWorkAfterLoop(ParameterInput *pin) {
 
       // Compute and store Laplacian of cell-centered conserved variables, Hydro and Bcc
       AthenaArray<Real> delta_cons_;
-      delta_cons_.NewAthenaArray(ncells4, ncells3, ncells2, ncells1);
+      delta_cons_.NewAthenaArray(ncells4, pmb->ncells3, pmb->ncells2, pmb->ncells1);
       pmb->pcoord->Laplacian(cons_, delta_cons_, il, iu, jl, ju, kl, ku, nl, nu);
 
       // TODO(felker): assuming uniform mesh with dx1f=dx2f=dx3f, so this factors out
@@ -442,13 +438,10 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
   // are set in InitUserMeshData
 
   if (MAGNETIC_FIELDS_ENABLED) {
-    AthenaArray<Real> a1,a2,a3;
-    int nx1 = (ie-is)+1 + 2*NGHOST;
-    int nx2 = (je-js)+1 + 2*NGHOST;
-    int nx3 = (ke-ks)+1 + 2*NGHOST;
-    a1.NewAthenaArray(nx3,nx2,nx1);
-    a2.NewAthenaArray(nx3,nx2,nx1);
-    a3.NewAthenaArray(nx3,nx2,nx1);
+    AthenaArray<Real> a1, a2, a3;
+    a1.NewAthenaArray(ncells3, ncells2, ncells1);
+    a2.NewAthenaArray(ncells3, ncells2, ncells1);
+    a3.NewAthenaArray(ncells3, ncells2, ncells1);
 
     // wave amplitudes
     dby = amp*rem[NWAVE-2][wave_flag];

src/pgen/magnoh.cpp
@@ -105,10 +105,8 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
 
   // initialize vector potential for inflowing B
   // (only initializing 2D array for vec potential)
-  int nx1 = (ie-is)+1 + 2*NGHOST;
-  int nx2 = (je-js)+1 + 2*NGHOST;
   AthenaArray<Real> az;
-  az.NewAthenaArray(nx2,nx1);
+  az.NewAthenaArray(ncells2, ncells1);
 
   for (int j=js; j<=je+1; ++j) {
     for (int i=is; i<=ie+1; ++i) {
@@ -136,8 +134,8 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
         if (std::strcmp(COORDINATE_SYSTEM, "cylindrical") == 0) {
           rad = pcoord->x1v(i);
         } else { // cartesian
-          x1=pcoord->x1v(i);
-          x2=pcoord->x2v(j);
+          x1 = pcoord->x1v(i);
+          x2 = pcoord->x2v(j);
           rad = std::sqrt(SQR(x1) + SQR(x2));
         }
         Real rho = rho0*std::pow(rad, alpha);

src/pgen/orszag_tang.cpp
@@ -44,9 +44,7 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
   Real gm1 = peos->GetGamma() - 1.0;
 
   AthenaArray<Real> az;
-  int nx1 = (ie-is)+1 + 2*NGHOST;
-  int nx2 = (je-js)+1 + 2*NGHOST;
-  az.NewAthenaArray(nx2,nx1);
+  az.NewAthenaArray(ncells2, ncells1);
 
   Real B0 = 1.0/std::sqrt(4.0*PI);
   Real d0 = 25.0/(36.0*PI);

src/outputs/history.cpp
@@ -14,6 +14,7 @@
 #include <cstdlib>
 #include <iomanip>
 #include <iostream>
+#include <memory>
 #include <sstream>
 #include <stdexcept>
 #include <string>
@@ -43,15 +44,12 @@ HistoryOutput::HistoryOutput(OutputParameters oparams)
 //  \brief Writes a history file
 
 void HistoryOutput::WriteOutputFile(Mesh *pm, ParameterInput *pin, bool flag) {
-  MeshBlock *pmb=pm->pblock;
+  MeshBlock *pmb = pm->pblock;
   AthenaArray<Real> vol;
-
-  int ncells1 = pmb->block_size.nx1 + 2*NGHOST;
-  vol.NewAthenaArray(ncells1);
-  int nhistory_output=NHISTORY_VARS+pm->nuser_history_output_;
-
-  Real *data_sum = new Real[nhistory_output];
-  for (int n=0; n<nhistory_output; ++n) data_sum[n]=0.0;
+  vol.NewAthenaArray(pmb->ncells1);
+  int nhistory_output = NHISTORY_VARS + pm->nuser_history_output_;
+  std::unique_ptr<Real[]> data_sum(new Real[nhistory_output]);
+  for (int n=0; n<nhistory_output; ++n) data_sum[n] = 0.0;
 
   // Loop over MeshBlocks
   while (pmb != nullptr) {
@@ -126,8 +124,8 @@ void HistoryOutput::WriteOutputFile(Mesh *pm, ParameterInput *pin, bool flag) {
     }
 
     // If this is the first output, write header
-    int iout = 1;
     if (output_params.file_number == 0) {
+      int iout = 1;
       std::fprintf(pfile,"# Athena++ history data\n"); // descriptor is first line
       std::fprintf(pfile,"# [%d]=time     ", iout++);
       std::fprintf(pfile,"[%d]=dt       ", iout++);
@@ -164,6 +162,5 @@ void HistoryOutput::WriteOutputFile(Mesh *pm, ParameterInput *pin, bool flag) {
   output_params.next_time += output_params.dt;
   pin->SetInteger(output_params.block_name, "file_number", output_params.file_number);
   pin->SetReal(output_params.block_name, "next_time", output_params.next_time);
-  delete [] data_sum;
   return;
 }

src/pgen/fft.cpp
@@ -47,21 +47,10 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
 //========================================================================================
 
 void Mesh::UserWorkAfterLoop(ParameterInput *pin) {
-  Coordinates *pcoord = pblock->pcoord;
-  Real x0=0.0, y0=0.0, z0=0.0;
-  int is=pblock->is, ie=pblock->ie;
-  int js=pblock->js, je=pblock->je;
-  int ks=pblock->ks, ke=pblock->ke;
-
   AthenaArray<Real> src, dst;
-  LogicalLocation &loc = pblock->loc;
-  RegionSize &block_size = pblock->block_size;
-  int nx1=block_size.nx1+2*NGHOST;
-  int nx2=block_size.nx2+2*NGHOST;
-  int nx3=block_size.nx3+2*NGHOST;
-
-  src.NewAthenaArray(nx3,nx2,nx1);
-  dst.NewAthenaArray(2,nx3,nx2,nx1);
+  // does this assume 3D? if not, perhaps should not use ncellsN
+  src.NewAthenaArray(pblock->ncells3, pblock->ncells2, pblock->ncells1);
+  dst.NewAthenaArray(2, pblock->ncells3, pblock->ncells2, pblock->ncells1);
 #ifdef FFT
   FFTDriver *pfftd;
   pfftd = new FFTDriver(this, pin);

commit ac3c34825f63055ff76e0110a356c3f9ab873c1d

Fix incorrect sizing of vector potential AthenaArrays in pgen/ files
Partially revert 9d803bef07cc4bb376b0768cd0da4895b128b709

Caused pernicious errors for 2D (but not 3D) MHD linear wave and CPAW
tests, since a3.NewAthenaArray(ncells3, ncells2, ncells1) did not extend
to required extent: nx3 = block_size.nx3 + 2*NGHOST

src/pgen/cpaw.cpp
@@ -225,9 +225,13 @@ void Mesh::UserWorkAfterLoop(ParameterInput *pin) {
 
 void MeshBlock::ProblemGenerator(ParameterInput *pin) {
   AthenaArray<Real> a1, a2, a3;
-  a1.NewAthenaArray(ncells3, ncells2, ncells1);
-  a2.NewAthenaArray(ncells3, ncells2, ncells1);
-  a3.NewAthenaArray(ncells3, ncells2, ncells1);
+  // nxN != ncellsN, in general. Allocate to extend through ghost zones, regardless # dim
+  int nx1 = block_size.nx1 + 2*NGHOST;
+  int nx2 = block_size.nx2 + 2*NGHOST;
+  int nx3 = block_size.nx3 + 2*NGHOST;
+  a1.NewAthenaArray(nx3, nx2, nx1);
+  a2.NewAthenaArray(nx3, nx2, nx1);
+  a3.NewAthenaArray(nx3, nx2, nx1);
 
   int level = loc.level;
   // Initialize components of the vector potential

src/pgen/field_loop.cpp
@@ -57,9 +57,13 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
   Real iso_cs =peos->GetIsoSoundSpeed();
 
   AthenaArray<Real> ax, ay, az;
-  ax.NewAthenaArray(ncells3, ncells2, ncells1);
-  ay.NewAthenaArray(ncells3, ncells2, ncells1);
-  az.NewAthenaArray(ncells3, ncells2, ncells1);
+  // nxN != ncellsN, in general. Allocate to extend through 2*ghost, regardless # dim
+  int nx1 = block_size.nx1 + 2*NGHOST;
+  int nx2 = block_size.nx2 + 2*NGHOST;
+  int nx3 = block_size.nx3 + 2*NGHOST;
+  ax.NewAthenaArray(nx3, nx2, nx1);
+  ay.NewAthenaArray(nx3, nx2, nx1);
+  az.NewAthenaArray(nx3, nx2, nx1);
 
   // Read initial conditions, diffusion coefficients (if needed)
   Real rad = pin->GetReal("problem","rad");
@@ -84,7 +88,7 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
     //     AND   lambda = x3size*sin_a;  are both satisfied.
 
     if (x1size == x3size) {
-      ang_2 = PI/4.0;
+      // ang_2 = PI/4.0;  // unused variable
       cos_a2 = sin_a2 = std::sqrt(0.5);
     } else {
       ang_2 = std::atan(x1size/x3size);
@@ -103,7 +107,7 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
   // the origin of the initial loop
   Real x0 = pin->GetOrAddReal("problem","x0",0.0);
   Real y0 = pin->GetOrAddReal("problem","y0",0.0);
-  Real z0 = pin->GetOrAddReal("problem","z0",0.0);
+  // Real z0 = pin->GetOrAddReal("problem","z0",0.0);
 
   for (int k=ks; k<=ke+1; k++) {
     for (int j=js; j<=je+1; j++) {

src/pgen/field_loop_poles.cpp
@@ -121,11 +121,15 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
   // Set initial magnetic fields
   if (MAGNETIC_FIELDS_ENABLED) {
     AthenaArray<Real> a1, a2, a3;
-    a1.NewAthenaArray(ncells3, ncells2, ncells1);
-    a2.NewAthenaArray(ncells3, ncells2, ncells1);
-    a3.NewAthenaArray(ncells3, ncells2, ncells1);
+    // nxN != ncellsN, in general. Allocate to extend through 2*ghost, regardless # dim
+    int nx1 = block_size.nx1 + 2*NGHOST;
+    int nx2 = block_size.nx2 + 2*NGHOST;
+    int nx3 = block_size.nx3 + 2*NGHOST;
+    a1.NewAthenaArray(nx3, nx2, nx1);
+    a2.NewAthenaArray(nx3, nx2, nx1);
+    a3.NewAthenaArray(nx3, nx2, nx1);
 
-    int level=loc.level;
+    int level = loc.level;
     for (int k=ks; k<=ke+1; k++) {
       for (int j=js; j<=je+1; j++) {
         for (int i=is; i<=ie+1; i++) {

src/pgen/linear_wave.cpp
@@ -439,9 +439,13 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
 
   if (MAGNETIC_FIELDS_ENABLED) {
     AthenaArray<Real> a1, a2, a3;
-    a1.NewAthenaArray(ncells3, ncells2, ncells1);
-    a2.NewAthenaArray(ncells3, ncells2, ncells1);
-    a3.NewAthenaArray(ncells3, ncells2, ncells1);
+    // nxN != ncellsN, in general. Allocate to extend through 2*ghost, regardless # dim
+    int nx1 = block_size.nx1 + 2*NGHOST;
+    int nx2 = block_size.nx2 + 2*NGHOST;
+    int nx3 = block_size.nx3 + 2*NGHOST;
+    a1.NewAthenaArray(nx3, nx2, nx1);
+    a2.NewAthenaArray(nx3, nx2, nx1);
+    a3.NewAthenaArray(nx3, nx2, nx1);
 
     // wave amplitudes
     dby = amp*rem[NWAVE-2][wave_flag];

src/pgen/magnoh.cpp
@@ -106,7 +106,7 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
   // initialize vector potential for inflowing B
   // (only initializing 2D array for vec potential)
   AthenaArray<Real> az;
-  az.NewAthenaArray(ncells2, ncells1);
+  az.NewAthenaArray(ncells2, ncells1);  // ncells2 is consistent only if 2D or 3D
 
   for (int j=js; j<=je+1; ++j) {
     for (int i=is; i<=ie+1; ++i) {

src/pgen/orszag_tang.cpp
@@ -44,7 +44,7 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
   Real gm1 = peos->GetGamma() - 1.0;
 
   AthenaArray<Real> az;
-  az.NewAthenaArray(ncells2, ncells1);
+  az.NewAthenaArray(ncells2, ncells1);  // ncells2 is consistent only if 2D or 3D
 
   Real B0 = 1.0/std::sqrt(4.0*PI);
   Real d0 = 25.0/(36.0*PI);